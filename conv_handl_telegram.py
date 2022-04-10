5533f45d36e9475f78802f158b41be8d021745a9
Dedstate <96666517+Dedstate@users.noreply.github.com>
2022-04-10 22:25:36 +0300

    Create conv_handl_telegram.py

---
+++ conv_handl_telegram.py
@@ -0,0 +1,120 @@
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Updater,
+    ConversationHandler,
+    CallbackQueryHandler,
+    CommandHandler,
+)
+from tokens import token
+from random import choice, randint
+from hero import Hero, items
+
+bot = Bot(token)
+updater = Updater(token, use_context=True)
+dispatcher = updater.dispatcher
+
+HELLO = 0
+BUTTON_CHECK = 1
+
+
+def start(update, context):
+    username = update.message.from_user["username"]
+    context.bot.send_message(update.effective_chat.id,
+                             f"Привет, о славный {username}")
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Этот мир погряз в боли и отчаянии и только ты можешь с этим справиться!",
+    )
+    start_button = [[
+        InlineKeyboardButton("Начать приключение!", callback_data="start")
+    ]]
+    update.message.reply_text("Готов ли ты дать им отпор",
+                              reply_markup=InlineKeyboardMarkup(start_button))
+    context.user_data["hero"] = Hero(username)
+    context.user_data["enemy"] = Hero("Георгий")
+    return HELLO
+
+
+def hello(update, context):
+    query = update.callback_query
+    if query.data == "start":
+        adventure = [
+            [InlineKeyboardButton("Пошарить вокруг", callback_data="search")],
+            [
+                InlineKeyboardButton("В поисках приключений!",
+                                     callback_data="enemy")
+            ],
+        ]
+        context.bot.send_message(
+            "Сделай свой выбор",
+            reply_markup=InlineKeyboardMarkup(adventure),
+        )
+    elif query.data == "search":
+        new_item = choice(items)
+        if new_item == "монеток":
+            new_money = choice([1, 3, 5, 10])
+            context.user_data["hero"].money += new_money
+            context.bot.send_message(
+                update.effective_chat.id,
+                f"Неплохой улов! Я обнаружил {new_money} монет, да я богат!",
+            )
+            update.effective_chat.id,
+        else:
+            context.user_data["hero"].add_equip(new_item)
+            context.bot.send_message(update.effective_chat.id,
+                                     f"Неплохой улов! Я обнаружил {new_item}")
+    elif query.data == "enemy":
+        variety = [
+            [
+                InlineKeyboardButton("Подкрасться со спины",
+                                     callback_data="sneak up from behind")
+            ],
+            [
+                InlineKeyboardButton("Подготовить засаду",
+                                     callback_data="ambush")
+            ],
+            [InlineKeyboardButton("Пойти напролом", callback_data="go ahead")],
+        ]
+        context.bot.send_message(
+            update.effective_chat.id,
+            "Сделай свой выбор",
+            reply_markup=InlineKeyboardMarkup(variety),
+        )
+        return BUTTON_CHECK
+
+
+print("Привет")
+
+
+def battle(update, context):
+    query = update.callback_query
+    if query.data == "sneak up from behind":
+        perc = randint(1, 100)
+    if perc >= 65:
+        ...
+
+
+enemy_equip = [
+    "стальной меч",
+    "мешок монет",
+    "геройский щит",
+    "комплект путешественника",
+]
+new_equip_f_enemy = choice(enemy_equip)
+rand_money = randint(10, 100)
+
+start_handler = CommandHandler("start", start)
+button_check_handler = CallbackQueryHandler(hello)
+conversation_handler = ConversationHandler(
+    entry_points=[start_handler],
+    states={
+        HELLO: [start_handler],
+        BUTTON_CHECK: [button_check_handler]
+    },
+    fallbacks=[],
+)
+
+dispatcher.add_handler(conversation_handler)
+
+updater.start_polling()
+updater.idle()
